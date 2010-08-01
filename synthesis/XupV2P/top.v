ad8b02f00380b27679256fd6126f26dafdac290b
nats <nats.fr@gmail.com>
2010-08-01 19:25:24 +0200

    Added working xupv2p design, working at 50MHz

--- synthesis/XupV2P/top.v
+++ synthesis/XupV2P/top.v
@@ -1,6 +1,8 @@
+`define CLOCK_FREQUENCY 50000000
+
 module top(input rewind_usart, output reset_led, input clock, input reset, output led, output led2, output led3, input UART_TXD, output UART_RXD);
 
-reg reset_sync;
+reg reset_sync = 1'b1;
 reg rewind_usart_sync;
 
 wire sys_clk_dcm;
@@ -9,10 +11,33 @@ wire sys_clk;
 wire clk_fb_buf;
 wire clk0_buf;
 
+wire not_led, not_led3;
+
+/* CHIPSCOPE PART ! */
+
+//wire [35:0] ILA_Ctrl_bus;
+//wire [31:0] data_ila;
+
+//assign data_ila = {23'b0, clock, sys_clk, not_led, led, not_led3, led3, led2, reset_led, rewind_usart_sync};
+
+//icon_module i_icon(.CONTROL0(ILA_Ctrl_bus)) /* synthesis syn_noprune=1 */;
+//ila_module i_ila(.CONTROL(ILA_Ctrl_bus), .CLK(sys_clk), .TRIG0({7'b0, reset_sync}), .DATA(data_ila)) /* synthesis syn_noprune=1 */;
+
+/* END OF CHIPSCOPE PART */
+
+/*wire locked_status;
+reg global_reset = 1'b1;
+
+reg locked_reg = 1'b0; // Based flip flop metastable filter.
+reg locked_reg_2 = 1'b0;
+
+reg startup_reset = 1'b1;
+reg [27:0] startup_rst_counter = 27'b0;*/
+
 DCM #(
 	.CLKDV_DIVIDE(2),
-	.CLKFX_DIVIDE(10),
-	.CLKFX_MULTIPLY(7),
+	.CLKFX_DIVIDE(4),
+	.CLKFX_MULTIPLY(2),
 	.CLKIN_DIVIDE_BY_2("FALSE"),
 	.CLKIN_PERIOD(10.0),
 	.CLKOUT_PHASE_SHIFT("NONE"),
@@ -33,10 +58,10 @@ DCM #(
 	.CLKDV(),
 	.CLKFX(sys_clk_dcm),
 	.CLKFX180(),
-	.LOCKED(),
+	.LOCKED(/*locked_status*/),
 	.CLKFB(clk_fb_buf),
 	.CLKIN(clock),
-	.RST(1'b0),
+	.RST(/*global_reset*/1'b0),
 	.PSEN(1'b0)
 );
 
@@ -50,15 +75,43 @@ BUFG b_fb(
 	.O(clk_fb_buf)
 );
 
+// DCM Watchdog
+//	Thanks to MatthiasM 
+/*always @(posedge clock)
+begin
+	reset_sync <= ~reset;
+	rewind_usart_sync <= ~rewind_usart;
+	locked_reg <= locked_status;
+	locked_reg_2 <= locked_reg;
+	global_reset <= reset_sync | startup_reset;
+	if(startup_rst_counter[10])
+	begin
+		if (locked_reg_2) begin
+			startup_reset <= 0;
+		end else begin
+			startup_rst_counter <= 16'b0;
+			startup_reset <= 1'b1;
+		end
+	end else begin
+		startup_rst_counter <= startup_rst_counter + 1;
+		startup_reset <= 1'b1;
+	end
+end*/
+
 always @(posedge sys_clk)
 begin
-	reset_sync <= reset;
-	rewind_usart_sync <= rewind_usart;
+	reset_sync <= ~reset;
+	rewind_usart_sync <= ~rewind_usart;
 end
 
-generator g1 (sys_clk, reset_sync, led, led3, UART_RXD, rewind_usart_sync);
+generator #(.clock_freq(`CLOCK_FREQUENCY)) g1 (sys_clk, reset_sync, not_led, not_led3, UART_RXD, rewind_usart_sync);
 
-assign led2 = UART_TXD;
-assign reset_led = reset_sync;
+assign reset_led = ~reset_sync; //~global_reset;
+assign led2 = ~rewind_usart_sync;
+assign led = ~not_led;
+assign led3 = ~not_led3;
+
+/*assign led2 = locked_reg_2;
+assign reset_led = global_reset;*/
 
 endmodule
